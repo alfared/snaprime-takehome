ad3247a5bfc93ff8e246670703ece3aeba9b4ce1
Vitaliy.Holovka <Vitaliy.Holovka@contracted.pmi.com>
2026-07-08 13:49:55 +0200

    SNAP-4: add html parser

---
+++ src/lib/extract/parser.ts
@@ -0,0 +1,35 @@
+import * as cheerio from "cheerio";
+
+export function parseHtml(url: string, html: string) {
+  const $ = cheerio.load(html);
+
+  return {
+    url,
+    title: $("title").text(),
+
+    description:
+      $('meta[name="description"]').attr("content") ??
+      $('meta[property="og:description"]').attr("content") ??
+      "",
+
+    headings: $("h1,h2,h3")
+        .map((_, el) => $(el).text())
+        .get(),
+
+    text: $("body")
+      .text()
+      .replace(/\s+/g, " ")
+      .trim()
+      .slice(0, 10000),
+
+    images: $("img")
+      .map((_, el) => $(el).attr("src"))
+      .get()
+      .filter(Boolean)
+      .map((src) => new URL(src!, url).href),
+
+    colors: [],
+
+    warnings: [],
+  }
+}
